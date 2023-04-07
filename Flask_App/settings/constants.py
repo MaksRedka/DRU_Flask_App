d88970442ec73633c02e0ae5f9536e9565ea738e
MaksRedka <mex4232@gmail.com>
2023-04-07 10:33:31 +0300

    Change folder name

--- Flask_App/settings/constants.py
+++ Flask_App/settings/constants.py
@@ -1,7 +1,7 @@
 import os
 # db connection URL (In order to submit your project do NOT change this value!!!)
-#DB_URL = os.environ['DB_URL'] 
-DB_URL = "postgresql+psycopg2://mex4232:58sahana@localhost:5432/test_db"                                                                  
+DB_URL = os.environ['DB_URL']     
+                                                            
 # entities properties
 ACTOR_FIELDS = ['id', 'name', 'gender', 'date_of_birth']
 MOVIE_FIELDS = ['id', 'name', 'year', 'genre']
